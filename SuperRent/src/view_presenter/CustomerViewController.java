8852346bac72b8fa34483e649fc4e726f08e3a98
nakisa6 <nakisa.abbasi@gmail.com>
2015-03-17 11:05:10 -0700

    started the customer page!

--- SuperRent/src/view_presenter/CustomerViewController.java
+++ SuperRent/src/view_presenter/CustomerViewController.java
@@ -7,21 +7,88 @@ package view_presenter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * 
- */
+
+
+
 public class CustomerViewController implements Initializable {
+    
+@FXML
+private ComboBox<String> LocationCombobox;
+ObservableList<String> LocationList=FXCollections.observableArrayList (
+        "Location-1","Location-2","Location-3");
+
+
+@FXML
+private ComboBox<String> VehicleTypeCombobox;
+ObservableList<String> VehicleList=FXCollections.observableArrayList (
+        "Car","Truck");
+
+
+@FXML
+private ComboBox<String> VehicleModelCombobox;
+ObservableList<String> CarModelList=FXCollections.observableArrayList (
+        "CarModel-1","CarModel-2");
+ObservableList<String> TruckModelList=FXCollections.observableArrayList (
+        "TruckModel-1","TruckModel-2");
+
+
+@FXML
+private ComboBox<String> PickUpTimeCombobox;
+@FXML
+private ComboBox<String> ReturnTimeCombobox;
+ObservableList<String> TimeList=FXCollections.observableArrayList (
+        "1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00");
+
+
+@FXML
+private ComboBox<String> TimeCombobox1;
+@FXML
+private ComboBox<String> TimeCombobox2;
+ObservableList<String> List=FXCollections.observableArrayList (
+        "AM","PM");
+
+
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        LocationCombobox.setItems(LocationList);
+        VehicleTypeCombobox.setItems(VehicleList);
+        PickUpTimeCombobox.setItems(TimeList);
+        ReturnTimeCombobox.setItems(TimeList);
+        TimeCombobox1.setItems(List);
+        TimeCombobox2.setItems(List);
+        
+        
+        
+        
+        
+       
+        
+        /*
+        if("Car".equals(VehicleTypeCombobox.getValue()))
+        {
+            VehicleModelCombobox.setItems(CarModelList);
+        }
+        else
+        {
+            VehicleModelCombobox.setItems(TruckModelList);
+      
+        }
+                */
     }    
     
 }
