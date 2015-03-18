41335f53c82fe007948fc919863fd6e165349085
eraserxp <eraser117xp@gmail.com>
2015-03-18 16:47:02 -0700

    add some comments (may be needed later)

--- SuperRent/src/model/UserModel.java
+++ SuperRent/src/model/UserModel.java
@@ -14,6 +14,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import static javafx.scene.control.TableView.CONSTRAINED_RESIZE_POLICY;
 import javafx.util.Callback;
 
 /**
@@ -77,7 +78,8 @@ public class UserModel {
                 });
 
                 tableview.getColumns().addAll(col);
-
+                // get rid of the extra column displayed in a Table View by default
+                // tableview.setColumnResizePolicy(CONSTRAINED_RESIZE_POLICY);
                 System.out.println("Column [" + i + "] ");
 
             }
