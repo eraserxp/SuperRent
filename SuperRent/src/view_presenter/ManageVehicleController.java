2fc343403ba7bc1da6ec68f80c29e8a4f22614e1
nakisa6 <nakisa.abbasi@gmail.com>
2015-03-31 19:53:46 -0700

    some changes.

--- SuperRent/src/view_presenter/ManageVehicleController.java
+++ SuperRent/src/view_presenter/ManageVehicleController.java
@@ -5,9 +5,20 @@
  */
 package view_presenter;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import model.AdminModel;
+import model.ManagerModel;
 
 /**
  * FXML Controller class
@@ -15,7 +26,24 @@ import javafx.fxml.Initializable;
  * @author NakMac
  */
 public class ManageVehicleController implements Initializable {
-
+    
+    @FXML 
+    private TextField typeTextField;
+    private TextField plateNumTextField;
+    private RadioButton carRadioButton;
+    private RadioButton truckRadioButton;
+    private Date startingDateDateBox;
+    private Button addButton;
+    private Button showButton;
+    private Button soldButton;
+   
+    
+    @FXML
+    private VBox vehicleForSaleVbox; // in show table tab
+    private TableView tableContent = null;
+    private AdminModel adminModel = new AdminModel();
+    private ManagerModel managerModel=new ManagerModel();
+   
     /**
      * Initializes the controller class.
      */
@@ -24,4 +52,16 @@ public class ManageVehicleController implements Initializable {
         // TODO
     }    
     
+     @FXML
+    private void handleShowButtonAction(ActionEvent event) throws IOException {
+    if (tableContent != null) {
+            vehicleForSaleVbox.getChildren().remove(tableContent);
+        }
+        adminModel.refeshDatabaseConnection();
+        tableContent = adminModel.getTable("vehicleforsale");
+        vehicleForSaleVbox.getChildren().add(tableContent);
+    }
+    
+    
+   
 }
