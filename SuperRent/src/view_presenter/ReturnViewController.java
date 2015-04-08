4ede4425a4cb9423c7f9193c63d7e23203fc9140
dongshengshen <dongshengshen@192.168.1.69>
2015-04-07 20:27:43 -0700

    Merge origin/master
    Conflicts:
    	SuperRent/src/model/UserModel.java

--- SuperRent/src/view_presenter/ReturnViewController.java
+++ SuperRent/src/view_presenter/ReturnViewController.java
@@ -123,6 +123,14 @@ public class ReturnViewController extends AbstractController implements Initiali
     ArrayList<Integer> quantities = new ArrayList<>();
     LocalDate fromDate, toDate;
 
+    LocalDate returnDate;
+    Integer rentidint,returnTimeInt,odometer;
+    
+    String rentid,city,location,customerusername,TankFull;
+    //payment method and total cost may not be available here
+    String Payment_Method = "Cash";
+    String totalcost = "";
+    Integer TankFullint = 0;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
@@ -153,20 +161,26 @@ public class ReturnViewController extends AbstractController implements Initiali
     private void showSummary() throws SQLException {
 
         String PlateNumString = PlateNoTextField.getText();
-        LocalDate returnDate = ReturnDatePicker.getValue();
+        returnDate = ReturnDatePicker.getValue();
         toDate = returnDate;
         String returnTimeString = ReturnTimeCombox.getSelectionModel().getSelectedItem().toString();
-        Integer returnTimeInt = Integer.parseInt(returnTimeString.split(":")[0]);
+        returnTimeInt = Integer.parseInt(returnTimeString.split(":")[0]);
         //Integer returnTime = Integer.parseInt(returnTimeString.split(":")[0]);
         //String OdometerReading = OdometerTextField.getText();
-        //String TankFull = TankFullCombox.getSelectionModel().getSelectedItem().toString();
+        TankFull = TankFullCombox.getSelectionModel().getSelectedItem().toString();
+        if(TankFull.equals("Yes")){
+            TankFullint = 1;
+            
+        }else{
+            TankFullint = 0;
+        }
         //String Equipment1Selection = Equip1Combox.getSelectionModel().getSelectedItem().toString();
         //String Equipment2Selection = Equip2Combox.getSelectionModel().getSelectedItem().toString();
 
         ArrayList<String> rentList = new ArrayList<>();
         rentList = clerkModel.getRentDetails(PlateNumString.trim());
         //System.out.println(rentList);
-        String customerusername = rentList.get(0);
+        customerusername = rentList.get(0);
         UsernameLabel.setText(customerusername);
         PlatenoLabel.setText(PlateNumString);
         String pickuplocation = rentList.get(2) + ", " + rentList.get(1);
@@ -185,13 +199,16 @@ public class ReturnViewController extends AbstractController implements Initiali
 
         ArrayList<String> clerkbranch = new ArrayList<>();
         clerkbranch = clerkModel.getClerkDetails(username);
+        city = clerkbranch.get(0);
+        location = clerkbranch.get(1);
         String returnlocation = clerkbranch.get(1) + ", " + clerkbranch.get(0);
         ReturnLocationLabel.setText(returnlocation);
 
         String returnTime = " at " + returnTimeString + " in " + returnDate;
         ReturnTimeLabel.setText(returnTime);
 
-        String rentid = rentList.get(5);
+        rentid = rentList.get(5);
+        rentidint =  Integer.parseInt(rentid);
         equipmentslist = new ArrayList<>();
         equipmentslist = clerkModel.getEquipmentDetails(rentid);
 
@@ -230,7 +247,7 @@ public class ReturnViewController extends AbstractController implements Initiali
 
         int redeemedPoints = 0;
         boolean isRoadStar = false;
-        int odometer = Integer.parseInt(OdometerTextField.getText());
+        odometer = Integer.parseInt(OdometerTextField.getText());
         if (!RoadStar.isDisabled() && RoadStar.isSelected()) {
             isRoadStar = true;
         }
@@ -253,6 +270,10 @@ public class ReturnViewController extends AbstractController implements Initiali
         summaryGP = userModel.calculateCost(vehicleType, equipments, quantities,
                 pickupDate, pickuptimeint, returnDate, returnTimeInt, isRoadStar,
                 redeemedPoints, odometer, PlateNumString,true );
+        totalcost = AppContext.getInstance().getTempData("amount");
+        Integer temp = Integer.parseInt(totalcost);
+        temp = temp/100;
+        totalcost = temp.toString();
         summaryVBox.getChildren().add(summaryGP);
         System.out.println(equipmentslist);
         //[child_safety_seat, 1, ski_rack, 1]
@@ -334,7 +355,16 @@ public class ReturnViewController extends AbstractController implements Initiali
             public void handle(ActionEvent e) {
 
                 try {
+                    
+                    
+                    clerkModel.createVreturn(rentidint,returnDate,returnTimeInt,city,location,TankFullint,odometer,totalcost,Payment_Method);
+
                     showPaymentDialog();
+                    
+                    
+                    
+                    
+                    
                 } catch (IOException ex) {
                     Logger.getLogger(ReturnViewController.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -345,6 +375,7 @@ public class ReturnViewController extends AbstractController implements Initiali
     }
 
     public void showPaymentDialog() throws IOException {
+
         AnchorPane loader = (AnchorPane) FXMLLoader.load(ReturnDialogViewController.class.getResource("PaymentCCView.fxml"));
 
         Stage dialogStage = new Stage();
@@ -354,12 +385,12 @@ public class ReturnViewController extends AbstractController implements Initiali
         dialogStage.setScene(scene);
 
         //do sth when the dialog is closed
-        dialogStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent we) {
-                System.out.println("Do sth when user click the X close icon of the dialog");
-
-            }
-        });
+//        dialogStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//            public void handle(WindowEvent we) {
+//                System.out.println("Do sth when user click the X close icon of the dialog");
+//
+//            }
+//        });
         dialogStage.showAndWait();
 
     }
