c3b92b2f8baa4602751830c9428ef7f1c9e4f58e
Namitha <Namitha@Namitha>
2015-04-12 03:27:05 -0700

    Membership Changes,Cancel and Show Reservation

--- SuperRent/src/model/CustomerModel.java
+++ SuperRent/src/model/CustomerModel.java
@@ -7,8 +7,10 @@ package model;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.control.TableView;
 
 /**
  *
@@ -43,6 +45,229 @@ public class CustomerModel extends UserModel {
         String changeAddress = "update customer set address = " + addQuotation(address)
                 + " where username = " + addQuotation(username);
         return updateDatabase(changeAddress);
+    }
+    
+    
+        
+    public boolean isReservationExist(String user_name) throws SQLException {
+        String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"'";
+        System.out.println(SQL);
+        
+        try {
+            
+        int count=0;
+        ResultSet rs=queryDatabase(SQL);
+        
+        while(rs.next()){
+            count++;
+            }
+            
+            rs.close();
+            if(count==0)
+                return false;
+            else 
+                return true;
+           }
+           
+           catch (Exception e) {
+
+            e.printStackTrace();
+
+            System.out.println("Error on Building Data");
+
+        }
+     return false;
+
+    }
+    
+    public boolean isReservationbyStatusExist(String user_name,String status) throws SQLException {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.status='"+status+"'";
+        System.out.println(SQL);
+        
+        try {
+            
+        int count=0;
+        ResultSet rs=queryDatabase(SQL);
+        
+        while(rs.next()){
+            count++;
+            }
+            
+            rs.close();
+            if(count==0)
+                return false;
+            else 
+                return true;
+           }
+           
+           catch (Exception e) {
+
+            e.printStackTrace();
+
+            System.out.println("Error on Building Data");
+
+        }
+     return false;
+
+    }
+    
+    public boolean isReservationbyCNExist(String user_name,int confno) throws SQLException {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.confirmation_number="+confno;
+        System.out.println(SQL);
+        
+        try {
+            
+        int count=0;
+        ResultSet rs=queryDatabase(SQL);
+        
+        while(rs.next()){
+            count++;
+            }
+            
+            rs.close();
+            if(count==0)
+                return false;
+            else 
+                return true;
+           }
+           
+           catch (Exception e) {
+
+            e.printStackTrace();
+
+            System.out.println("Error on Building Data");
+
+        }
+     return false;
+
+    }
+    
+    public TableView getReservationsbyCN (String user_name,int confno) {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.confirmation_number="+confno;
+        System.out.println(SQL);
+        return getTableViewForSQL(SQL);
+
+    }
+    
+    public boolean isReservationbyCNSExist(String user_name,int confno,String status) throws SQLException {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.confirmation_number="+confno+" and R.status='"+status+"'";
+        System.out.println(SQL);
+        
+        try {
+            
+        int count=0;
+        ResultSet rs=queryDatabase(SQL);
+        
+        while(rs.next()){
+            count++;
+            }
+            
+            rs.close();
+            if(count==0)
+                return false;
+            else 
+                return true;
+           }
+           
+           catch (Exception e) {
+
+            e.printStackTrace();
+
+            System.out.println("Error on Building Data");
+
+        }
+     return false;
+
+    }
+    
+    public TableView getReservationsbyCNS (String user_name,int confno,String status) {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.confirmation_number="+confno+" and R.status='"+status+"'";
+        System.out.println(SQL);
+        return getTableViewForSQL(SQL);
+
+    }
+    
+     
+    public TableView getReservationsbyStatus(String user_name,String status) {
+         String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"' and R.status='"+status+"'";
+        System.out.println(SQL);
+        return getTableViewForSQL(SQL);
+
+    }
+    
+    public TableView getallReservations(String user_name) {
+        String SQL = "select R.confirmation_number as 'Confirmation No.', "
+                + "R.pickup_date as 'Pickup date', "
+                + " R.pickup_time as 'Pickup hour', "
+                + "R.return_date as 'Return date', "
+                + " R.return_time as 'Return hour', "
+                + " R.branch_city as City, R.branch_location as Location, R.status as Status"
+                + " from reservation R "
+                + " where R.customer_username='"+user_name+"'";
+        System.out.println(SQL);
+        return getTableViewForSQL(SQL);
+
+    }
+    
+    public boolean cancelreservation(String username,int confno, String status, String category) throws SQLException {
+
+        String deleteAddon = "delete from reserve_addon where confirmNo = " + confno;
+        System.out.println(" \n" + deleteAddon);
+
+        String changereserve = "update reservation set status ='expired'"
+                + " where confirmation_number = " + confno;
+        
+        return updateDatabaseBatch(deleteAddon, changereserve);
+
     }
     
 }
